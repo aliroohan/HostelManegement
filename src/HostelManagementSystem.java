027b7af83a7ab29b32614836be105a26ecdbd26d
aliroohan <aliroohan321@gmail.com>
2024-05-11 23:40:19 +0500

    Added display and edit methods

--- src/HostelManagementSystem.java
+++ src/HostelManagementSystem.java
@@ -8,40 +8,24 @@ public class HostelManagementSystem {
         Student student3 = new Student("S003", "Doe", 19, "Male", "0123456789", 103);
         HostelManager hostelManager = new HostelManager();
         Hostel hostel1 = new Hostel("H001", "Hostel A", "123 Main Street");
+        Hostel hostel2 = new Hostel("H002", "Hostel B", "321 Main Street");
         Room room1 = new Room(101, 4);
+        Room room2 = new Room(102, 4);
         hostel1.getRooms().add(room1);
-//        hostelManager.addStudent(student1, hostel1);
-//        hostelManager.addStudent(student2, hostel1);
-//        hostelManager.addStudent(student3, hostel1);
-//        FileReader fileReader = null;
-//        try {
-//            fileReader = new FileReader("Hostel A.txt");
-//            Scanner scanner = new Scanner(fileReader);
-//            while (scanner.hasNextLine()) {
-//                String [] student = scanner.nextLine().split(",");
-//                System.out.println("Student ID: " + student[0]);
-//                System.out.println("Name: " + student[1]);
-//                System.out.println("Age: " + student[2]);
-//                System.out.println("Gender: " + student[3]);
-//                System.out.println("Phone number: " + student[4]);
-//                System.out.println("Room number: " + student[5]);
-//                System.out.println();
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (fileReader != null) {
-//                    fileReader.close();
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        hostel2.getRooms().add(room2);
+        hostelManager.getHostels().add(hostel1);
+        hostelManager.getHostels().add(hostel2);
 
-        hostelManager.searchStudent("S001", hostel1);
-        hostelManager.searchStudent("S002", hostel1);
-        hostelManager.removeStudent(student3, hostel1);
-        hostelManager.searchStudent("S003", hostel1);
+
+//        hostelManager.addStudent(student3, hostel2);
+//        hostelManager.editDetails(student1, hostel1);
+        hostelManager.editDetails(student3, hostel1);
+//        hostelManager.displayAllStudents();
+//        hostelManager.displayStudents(hostel1);
+        hostelManager.displayStudents(hostel2);
+//        hostelManager.searchStudent("S001", hostel1);
+//        hostelManager.searchStudent("S002", hostel1);
+//        hostelManager.removeStudent(student3, hostel2);
+//        hostelManager.searchStudent("S003", hostel1);
     }
 }
