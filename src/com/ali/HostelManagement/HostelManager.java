41e3eeefd3ee9eda5a7cc2c49d5b7e01aa088255
aliroohan <aliroohan321@gmail.com>
2024-05-19 12:30:45 +0500

    Updated removeStudent method

--- src/com/ali/HostelManagement/HostelManager.java
+++ src/com/ali/HostelManagement/HostelManager.java
@@ -183,6 +183,7 @@ public class HostelManager {
             System.out.println();
         }
     }
+
     public boolean checkDuplicateStudentID(String studentID) {
         for (Hostel hostel : hostels) {
             if (!hostel.getStudents().isEmpty()) {
@@ -218,27 +219,34 @@ public class HostelManager {
 
     public void removeStudent(Student student, Hostel hostel) {
         // Remove student from hostel
+        this.removeStudentFromHostelFile(student, hostel);
+        hostels.get(hostels.indexOf(hostel)).getStudents().remove(student);
+    }
+
+    public void removeStudentFromHostelFile(Student student, Hostel hostel) {
+        ArrayList<Student> tempStudents = new ArrayList<>();
         FileReader fileReader = null;
-        File tempFile = new File("temp.txt");
-        File hostelFile = new File(hostel.getName() + ".txt");
         try {
-            tempFile.createNewFile();
             fileReader = new FileReader(hostel.getName() + ".txt");
             Scanner scanner = new Scanner(fileReader);
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile));
             while (scanner.hasNextLine()) {
                 String[] studentInfo = scanner.nextLine().split(",");
                 if (!studentInfo[0].equals(student.getStudentID())) {
-                    bufferedWriter.write(studentInfo[0] + "," + studentInfo[1] + "," + studentInfo[2] + "," + studentInfo[3] + "," + studentInfo[4] + "," + studentInfo[5]);
-                    bufferedWriter.newLine();
+                    tempStudents.add(new Student(studentInfo[0], studentInfo[1], Integer.parseInt(studentInfo[2]), studentInfo[3], studentInfo[4], Integer.parseInt(studentInfo[5])));
                 }
             }
-            bufferedWriter.close();
             fileReader.close();
-            hostelFile.delete();
-            if (!tempFile.renameTo(new File(hostel.getName() + ".txt"))) {
-                throw new RuntimeException("Could not rename file");
+            FileWriter writer = new FileWriter(hostel.getName() + ".txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(tempStudents.getFirst().toString());
+            bufferedWriter.newLine();
+            tempStudents.removeFirst();
+            for (Student tempStudent : tempStudents) {
+                bufferedWriter.write(tempStudent.toString());
+                bufferedWriter.newLine();
             }
+            bufferedWriter.close();
+            writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
@@ -250,6 +258,29 @@ public class HostelManager {
                 throw new RuntimeException(e);
             }
         }
+
+    }
+    public Hostel searchHostelByStudentID(String studentID) {
+        // Search for hostel by student ID
+        for (Hostel hostel : hostels) {
+            for (Student student : hostel.getStudents()) {
+                if (student.getStudentID().equals(studentID)) {
+                    return hostel;
+                }
+            }
+        }
+        return null;
+    }
+    public Student searchStudent(String studentID) {
+        // Search for student in hostel
+        for (Hostel hostel : hostels) {
+            for (Student student : hostel.getStudents()) {
+                if (student.getStudentID().equals(studentID)) {
+                    return student;
+                }
+            }
+        }
+        return null;
     }
 
     public void displayStudents(Hostel hostel) {
@@ -384,42 +415,5 @@ public class HostelManager {
         }
     }
 
-    public void searchStudent(String studentID, Hostel hostel) {
-        // Search for student in hostel
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(hostel.getName() + ".txt");
-            Scanner scanner = new Scanner(fileReader);
-            boolean studentFound = false;
-            while (scanner.hasNextLine()) {
-                String[] student = scanner.nextLine().split(",");
-                if (student[0].equals(studentID)) {
-                    System.out.println("Student ID: " + student[0]);
-                    System.out.println("Name: " + student[1]);
-                    System.out.println("Age: " + student[2]);
-                    System.out.println("Gender: " + student[3]);
-                    System.out.println("Phone number: " + student[4]);
-                    System.out.println("Room number: " + student[5]);
-                    System.out.println();
-                    studentFound = true;
-                    break;
-                }
-            }
-            if (!studentFound) {
-                System.out.println("Student not found.");
-            }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (fileReader != null) {
-                    fileReader.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-    }
 }
